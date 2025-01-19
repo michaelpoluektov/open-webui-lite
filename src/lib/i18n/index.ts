c35b63ce1ca8cc763e619f75d502a203e8fa7879
Michael Poluektov <michael.poluektov@gmail.com>
2025-01-19 16:33:32 +0000

    format and remove ollama docker builds

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -5,75 +5,75 @@ import resourcesToBackend from 'i18next-resources-to-backend';
 import { writable } from 'svelte/store';
 
 const createI18nStore = (i18n: i18nType) => {
-  const i18nWritable = writable(i18n);
+	const i18nWritable = writable(i18n);
 
-  i18n.on('initialized', () => {
-    i18nWritable.set(i18n);
-  });
-  i18n.on('loaded', () => {
-    i18nWritable.set(i18n);
-  });
-  i18n.on('added', () => i18nWritable.set(i18n));
-  i18n.on('languageChanged', () => {
-    i18nWritable.set(i18n);
-  });
-  return i18nWritable;
+	i18n.on('initialized', () => {
+		i18nWritable.set(i18n);
+	});
+	i18n.on('loaded', () => {
+		i18nWritable.set(i18n);
+	});
+	i18n.on('added', () => i18nWritable.set(i18n));
+	i18n.on('languageChanged', () => {
+		i18nWritable.set(i18n);
+	});
+	return i18nWritable;
 };
 
 const createIsLoadingStore = (i18n: i18nType) => {
-  const isLoading = writable(false);
+	const isLoading = writable(false);
 
-  // if loaded resources are empty || {}, set loading to true
-  i18n.on('loaded', (resources) => {
-    // console.log('loaded:', resources);
-    Object.keys(resources).length !== 0 && isLoading.set(false);
-  });
+	// if loaded resources are empty || {}, set loading to true
+	i18n.on('loaded', (resources) => {
+		// console.log('loaded:', resources);
+		Object.keys(resources).length !== 0 && isLoading.set(false);
+	});
 
-  // if resources failed loading, set loading to true
-  i18n.on('failedLoading', () => {
-    isLoading.set(true);
-  });
+	// if resources failed loading, set loading to true
+	i18n.on('failedLoading', () => {
+		isLoading.set(true);
+	});
 
-  return isLoading;
+	return isLoading;
 };
 
 export const initI18n = (defaultLocale: string | undefined) => {
-  const detectionOrder = defaultLocale
-    ? ['querystring', 'localStorage']
-    : ['querystring', 'localStorage', 'navigator'];
-  const fallbackDefaultLocale = defaultLocale ? [defaultLocale] : ['en-US'];
+	const detectionOrder = defaultLocale
+		? ['querystring', 'localStorage']
+		: ['querystring', 'localStorage', 'navigator'];
+	const fallbackDefaultLocale = defaultLocale ? [defaultLocale] : ['en-US'];
 
-  const loadResource = (language: string, namespace: string) =>
-    import(`./locales/${language}/${namespace}.json`);
+	const loadResource = (language: string, namespace: string) =>
+		import(`./locales/${language}/${namespace}.json`);
 
-  i18next
-    .use(resourcesToBackend(loadResource))
-    .use(LanguageDetector)
-    .init({
-      debug: false,
-      detection: {
-        order: detectionOrder,
-        caches: ['localStorage'],
-        lookupQuerystring: 'lang',
-        lookupLocalStorage: 'locale'
-      },
-      fallbackLng: {
-        default: fallbackDefaultLocale
-      },
-      ns: 'translation',
-      returnEmptyString: false,
-      interpolation: {
-        escapeValue: false // not needed for svelte as it escapes by default
-      }
-    });
+	i18next
+		.use(resourcesToBackend(loadResource))
+		.use(LanguageDetector)
+		.init({
+			debug: false,
+			detection: {
+				order: detectionOrder,
+				caches: ['localStorage'],
+				lookupQuerystring: 'lang',
+				lookupLocalStorage: 'locale'
+			},
+			fallbackLng: {
+				default: fallbackDefaultLocale
+			},
+			ns: 'translation',
+			returnEmptyString: false,
+			interpolation: {
+				escapeValue: false // not needed for svelte as it escapes by default
+			}
+		});
 };
 
 const i18n = createI18nStore(i18next);
 const isLoadingStore = createIsLoadingStore(i18next);
 
 export const getLanguages = async () => {
-  const languages = (await import(`./locales/languages.json`)).default;
-  return languages;
+	const languages = (await import(`./locales/languages.json`)).default;
+	return languages;
 };
 export default i18n;
 export const isLoading = isLoadingStore;
